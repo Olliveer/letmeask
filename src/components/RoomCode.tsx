520b2b4887a4023eeeecf418ce735ce3e8a57d6c
José Oliveira <j.oliveira90@live.com>
2021-06-23 15:25:25 -0300

    add RoomCode component

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,28 @@
+import compyImg from '../assets/images/copy.svg';
+
+import '../styles/roomCode.scss';
+
+type RoomCodeProps = {
+  code: string;
+}
+
+function RoomCode({ code }: RoomCodeProps) {
+  function copyRoomCodeToClipBoard() {
+    navigator.clipboard.writeText(code);
+  }
+
+  return (
+    <button className="room-code" onClick={copyRoomCodeToClipBoard}>
+      <div>
+        <img src={compyImg} alt="Copy roomm code" />
+      </div>
+
+      <span>
+        Sala #
+        {code}
+      </span>
+    </button>
+  );
+}
+
+export { RoomCode };
